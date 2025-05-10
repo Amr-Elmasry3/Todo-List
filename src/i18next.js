a26134c6f786ea54902e33bff886a95c2d87c08d
Amr Elmasry <www.amrelmasry66@gmail.com>
2025-05-10 22:10:52 +0300

    update i18next file

--- src/i18next.js
+++ src/i18next.js
@@ -21,10 +21,16 @@ i18n
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
     fallbackLng: "en",
-    debug: true,
+    debug: false,
 
     interpolation: {
       escapeValue: false, // not needed for react as it escapes by default
+    },
+    backend: {
+      loadPath:
+        process.env.NODE_ENV === "production"
+          ? "/Todo-List/locales/{{lng}}/translation.json"
+          : "/locales/{{lng}}/translation.json",
     },
   });
 
